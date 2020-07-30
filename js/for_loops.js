0b5482faa2b2ae8bef8c532a944031183fc9a2bd
Caroline Esquivel <carolineesquivel@Carolines-MBP.lan>
2020-07-30 16:15:40 -0500

    for loops complete

--- js/for_loops.js
+++ js/for_loops.js
@@ -13,9 +13,21 @@
 
     console.log(showMultiplicationTable(7));
 
+ function getRandomInt(min, max) {
+     return Math.floor(Math.random() * (max - min +1) + min);
+ }
 
-    for (var i = 1; i <= 10; i++) {
-        var random = Math.floor(Math.random() * 200) + 20;
+ // for var i = 1; i <= 10; i++) {
+ //    var randomNumber = getRandomInt (20, 200);
+ //    if (randomNumber % 2 === 0) {
+ //        console.log(randomNumber + ' even');
+ //    } else {
+ //        console.log (randomNumber + ' odd');
+ //    }
+ // }
+    //Another one:
+ for (var i = 1; i <= 10; i++) {
+        var random = Math.floor(Math.random() * 180) + 20;
         console.log(random);
         if (random % 2 === 0) {
             console.log(random + " is even");
@@ -27,7 +39,15 @@
     for (var i = 1; i <= 9; i++) {
         console.log(i.toString().repeat([i]));
     }
-
+//
+// for (var i = 1; i <= 9; i++) {
+//     var result = "";
+//     for (var j = 0; j <= i; j++) {
+//         result += i;
+//     }
+//     console.log(result);
+// }
+    //or...
 
     for (var i = 100; i >= 5; i -= 5) {
         if (i % 5 === 0) {
